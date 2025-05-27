2b85db64d422e5b0855eb32d82478619169a3eb1
Andthehand <sixdefords50@gmail.com>
2025-05-26 23:00:39 -0400

    Added header type packets

--- Drone_Controller.c
+++ Drone_Controller.c
@@ -16,22 +16,29 @@
 typedef struct TCP_SERVER_T_ {
     struct tcp_pcb *server_pcb;
     struct tcp_pcb *client_pcb;
-    bool complete;
-    uint8_t buffer_sent[BUF_SIZE];
-    uint8_t buffer_recv[BUF_SIZE];
-    int sent_len;
-    int recv_len;
+    bool heartbeat_received;
+    bool isConnected;
     int run_count;
 } TCP_SERVER_T;
 
-static TCP_SERVER_T* tcp_server_init(void) {
-    TCP_SERVER_T *state = calloc(1, sizeof(TCP_SERVER_T));
-    if (!state) {
-        DEBUG_printf("failed to allocate state\n");
-        return NULL;
-    }
-    return state;
-}
+typedef uint32_t HeaderType_T;
+enum HeaderType_T_ {
+	HeartBeat = 0,
+    RequestDroneData = 1,
+	DroneData = 2,
+};
+
+
+typedef struct DroneData_T_ {
+	float roll;
+	float pitch;
+	float yaw;
+	float temp;
+} DroneData_T;
+
+typedef struct Header_T_ {
+	HeaderType_T type;
+} Header_T;
 
 static err_t tcp_server_close(void *arg) {
     TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
@@ -58,113 +65,99 @@ static err_t tcp_server_close(void *arg) {
     return err;
 }
 
-static err_t tcp_server_result(void *arg, int status) {
-    TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    if (status == 0) {
-        DEBUG_printf("test success\n");
-    } else {
-        DEBUG_printf("test failed %d\n", status);
-    }
-    state->complete = true;
-    return tcp_server_close(arg);
-}
-
 static err_t tcp_server_sent(void *arg, struct tcp_pcb *tpcb, u16_t len) {
     TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
     DEBUG_printf("tcp_server_sent %u\n", len);
-    state->sent_len += len;
-
-    if (state->sent_len >= BUF_SIZE) {
-
-        // We should get the data back from the client
-        state->recv_len = 0;
-        DEBUG_printf("Waiting for buffer from client\n");
-    }
 
     return ERR_OK;
 }
 
-err_t tcp_server_send_data(void *arg, struct tcp_pcb *tpcb)
-{
-    TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    for(int i=0; i< BUF_SIZE; i++) {
-        state->buffer_sent[i] = rand();
+err_t tcp_server_send_data(void* data, int length, struct tcp_pcb *tpcb) {
+    DEBUG_printf("Writing %ld bytes to client\n", length);
+    DEBUG_printf("Data: ");
+    for (int i = 0; i < length; i++) {
+        DEBUG_printf("%02x ", ((uint8_t*)data)[i]);
     }
-
-    state->sent_len = 0;
-    DEBUG_printf("Writing %ld bytes to client\n", BUF_SIZE);
     // this method is callback from lwIP, so cyw43_arch_lwip_begin is not required, however you
     // can use this method to cause an assertion in debug mode, if this method is called when
     // cyw43_arch_lwip_begin IS needed
     cyw43_arch_lwip_check();
-    err_t err = tcp_write(tpcb, state->buffer_sent, BUF_SIZE, TCP_WRITE_FLAG_COPY);
-    if (err != ERR_OK) {
-        DEBUG_printf("Failed to write data %d\n", err);
-        return tcp_server_result(arg, -1);
-    }
-    return ERR_OK;
+    return tcp_write(tpcb, data, length, TCP_WRITE_FLAG_COPY);
 }
 
 err_t tcp_server_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err) {
     TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
     if (!p) {
-        return tcp_server_result(arg, -1);
+        DEBUG_printf("tcp_server_recv pbuf is NULL\n");
+        state->isConnected = false;
+        return ERR_VAL;
     }
     // this method is callback from lwIP, so cyw43_arch_lwip_begin is not required, however you
     // can use this method to cause an assertion in debug mode, if this method is called when
     // cyw43_arch_lwip_begin IS needed
     cyw43_arch_lwip_check();
     if (p->tot_len > 0) {
-        DEBUG_printf("tcp_server_recv %d/%d err %d\n", p->tot_len, state->recv_len, err);
+        DEBUG_printf("tcp_server_recv %d err %d\n", p->tot_len, err);
 
-        // Receive the buffer
-        const uint16_t buffer_left = BUF_SIZE - state->recv_len;
-        state->recv_len += pbuf_copy_partial(p, state->buffer_recv + state->recv_len,
-                                             p->tot_len > buffer_left ? buffer_left : p->tot_len, 0);
+        Header_T *header = (Header_T*)p->payload;
+        if (header->type == HeartBeat) {
+            DEBUG_printf("Heartbeat received\n");
+            state->heartbeat_received = true;
+        } else if (header->type == DroneData) {
+            DEBUG_printf("DroneData Should not be recieved ever\n");
+        } else if (header->type == RequestDroneData) {
+            DEBUG_printf("Requesting Drone Data\n");
+            DroneData_T droneData = {0};
+            droneData.roll = 1.0f;
+            droneData.pitch = 2.0f;
+            droneData.yaw = 3.0f;
+            droneData.temp = 4.0f;
+
+            Header_T header = {DroneData};
+            tcp_server_send_data(&header, sizeof(header), tpcb);
+            tcp_server_send_data(&droneData, sizeof(droneData), tpcb);
+        } else {
+            DEBUG_printf("Unknown header type %d\n", header->type);
+        }
         tcp_recved(tpcb, p->tot_len);
     }
     pbuf_free(p);
-
-    // Have we have received the whole buffer
-    if (state->recv_len == BUF_SIZE) {
-
-        // check it matches
-        if (memcmp(state->buffer_sent, state->buffer_recv, BUF_SIZE) != 0) {
-            DEBUG_printf("buffer mismatch\n");
-            return tcp_server_result(arg, -1);
-        }
-        DEBUG_printf("tcp_server_recv buffer ok\n");
-
-        // Test complete?
-        state->run_count++;
-        if (state->run_count >= TEST_ITERATIONS) {
-            tcp_server_result(arg, 0);
-            return ERR_OK;
-        }
-
-        // Send another buffer
-        return tcp_server_send_data(arg, state->client_pcb);
-    }
     return ERR_OK;
 }
 
+// This function is called when the TCP connection is idle for a while
 static err_t tcp_server_poll(void *arg, struct tcp_pcb *tpcb) {
     DEBUG_printf("tcp_server_poll_fn\n");
-    return tcp_server_result(arg, -1); // no response is an error?
+    TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
+
+    //Make sure the client is still connected
+    if(state->heartbeat_received) {
+        static Header_T header = {HeartBeat};
+
+        tcp_server_send_data(&header, sizeof(header), tpcb);
+        state->heartbeat_received = false;
+        return ERR_OK;
+    } else {
+        DEBUG_printf("Heartbeat not received\n");
+        state->isConnected = false;
+    }
+
+    return ERR_TIMEOUT; // no response is an error?
 }
 
 static void tcp_server_err(void *arg, err_t err) {
+    TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
     if (err != ERR_ABRT) {
         DEBUG_printf("tcp_client_err_fn %d\n", err);
-        tcp_server_result(arg, err);
+        
+        state->isConnected = false;
     }
 }
 
 static err_t tcp_server_accept(void *arg, struct tcp_pcb *client_pcb, err_t err) {
     TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    if (err != ERR_OK || client_pcb == NULL) {
-        DEBUG_printf("Failure in accept\n");
-        tcp_server_result(arg, err);
+    if (err != ERR_OK || client_pcb == NULL) {\
+        DEBUG_printf("Failure to accept client connection\n");
         return ERR_VAL;
     }
     DEBUG_printf("Client connected\n");
@@ -176,7 +169,7 @@ static err_t tcp_server_accept(void *arg, struct tcp_pcb *client_pcb, err_t err)
     tcp_poll(client_pcb, tcp_server_poll, POLL_TIME_S * 2);
     tcp_err(client_pcb, tcp_server_err);
 
-    return tcp_server_send_data(arg, state->client_pcb);
+    return ERR_OK;
 }
 
 static bool tcp_server_open(void *arg) {
@@ -210,25 +203,32 @@ static bool tcp_server_open(void *arg) {
     return true;
 }
 
-void run_tcp_server_test(void) {
-    TCP_SERVER_T *state = tcp_server_init();
+void run_tcp_server(void) {
+    TCP_SERVER_T *state = calloc(1, sizeof(TCP_SERVER_T));
+    state->heartbeat_received = true;
+    state->isConnected = true;
     if (!state) {
+        DEBUG_printf("failed to allocate state\n");
         return;
     }
     if (!tcp_server_open(state)) {
-        tcp_server_result(state, -1);
+        DEBUG_printf("failed to open server\n");
         return;
     }
-    while(!state->complete) {
+    while(state->isConnected) {
         // This sleep is just an example of some (blocking)
         // work you might be doing.
         sleep_ms(1000);
     }
+
+    DEBUG_printf("Closing server\n");
+    tcp_server_close(state);
     free(state);
 }
 
 int main() {
     stdio_init_all();
+    sleep_ms(1000);
 
     if (cyw43_arch_init()) {
         printf("failed to initialise\n");
@@ -244,7 +244,7 @@ int main() {
     } else {
         printf("Connected.\n");
     }
-    run_tcp_server_test();
+    run_tcp_server();
     cyw43_arch_deinit();
     return 0;
 }
